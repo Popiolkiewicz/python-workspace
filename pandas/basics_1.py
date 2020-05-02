b1a1648add950c4c3c37cafe92a6902b5400a75a
Hubert Popiołkiewicz <popiolkiewicz.it@gmail.com>
2020-05-02 20:18:19 +0200

    Pandas basics

--- pandas/basics_1.py
+++ pandas/basics_1.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("avocado.csv")
+df = df.copy()[ df["type"] == "organic"]
 df['Date'] = pd.to_datetime(df["Date"])
 
 # print(df.head(10))
@@ -31,4 +32,22 @@ albany_df['price25ma'].plot()
 
 albany_df.dropna().head(3)
 
+# list(set(df['region'].values.tolist()))
+uniqueRegion = df['region'].unique()
+
+graph_df = pd.DataFrame()
+
+for region in uniqueRegion:
+    print(region)
+    region_df = df.copy()[ df['region'] == region ]
+    region_df.set_index("Date", inplace=True)
+    region_df.sort_index(inplace=True)
+    region_df[f'{region}_price25ma'] = region_df['AveragePrice'].rolling(25).mean()
+
+    if graph_df.empty:
+        graph_df = region_df[[f'{region}_price25ma']]
+    else:
+        graph_df = graph_df.join(region_df[f'{region}_price25ma'])
+
+graph_df.dropna().plot(figsize=(8,5), legend=False)
 plt.show()
